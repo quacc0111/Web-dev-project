c709b0490de69a7dbdac2ee609d0c0b7c42aae0f
quacc0111 <mcmohammed120@gmail.com>
2025-03-30 01:30:49 +0300

    done with 3 pages (i think so)

---
+++ js/Admin.js
@@ -0,0 +1,28 @@
+async function loadPage() {
+    const mainContent = document.getElementById("coursescards");
+  
+    const response = await fetch("/js/courses.json");
+    const courses = await response.json();
+  
+    courses.forEach(course => {
+      const card = document.createElement("div");
+      card.classList.add("course-card");
+  
+      card.innerHTML = `
+        <div class="course-metadata">
+          <div class="course-name">${course.course_name}</div>
+          <div class="course-instructor">${course.instructor}</div>
+          <div class="course-time">${course.time}</div>
+          <div class="course-other">${course.details}</div>
+          <div class="course-prereq">${course.prerequisites}</div>
+          <button class="bottom-button">List Students</button>
+
+        </div>
+      `;
+  
+      mainContent.appendChild(card);
+    });
+  }
+  
+  loadPage();
+  
